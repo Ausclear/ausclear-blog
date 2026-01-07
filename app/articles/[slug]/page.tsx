b3026a6185af93c94e9c46df78fe76c74a484d36
Ausclear <ausclear.test@gmail.com>
2026-01-07 13:08:33 +1030

    Reduce revalidate from 1 hour to 5 minutes

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -5,7 +5,7 @@ import { notFound } from 'next/navigation'
 import Link from 'next/link'
 import FeedbackButtons from '@/components/FeedbackButtons'
 
-export const revalidate = 3600
+export const revalidate = 300 // Revalidate every 5 minutes
 
 type Props = {
   params: { slug: string }
