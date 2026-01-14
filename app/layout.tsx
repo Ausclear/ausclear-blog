7074eafbb781edc95cf7e58735225cfeb9053888
Ausclear <ausclear.test@gmail.com>
2026-01-14 15:32:30 +1030

    TEMP: Disable live chat widget until testing complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import AnthonyAIWidget from '@/components/AnthonyAIWidget'
+// import AnthonyAIWidget from '@/components/AnthonyAIWidget' // TEMPORARILY DISABLED
 
 export const metadata: Metadata = {
   title: {
@@ -58,7 +58,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
-        <AnthonyAIWidget />
+        {/* <AnthonyAIWidget /> */} {/* TEMPORARILY DISABLED */}
       </body>
     </html>
   )
